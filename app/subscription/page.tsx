a19160f2ca45f1ea7d8578ff6a85f735b260ea4e
João Vinicius <joaodessimonifernandes@outlook.com>
2024-11-09 12:13:08 -0300

    feat: add navbar components

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,11 @@
+import Navbar from "../_components/navbar";
+
 const SubscriptionPage = () => {
-    return <h1>Pagina Subscription</h1>;
-}
- 
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
 export default SubscriptionPage;
